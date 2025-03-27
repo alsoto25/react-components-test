65ebaa794a8a49991af77ec5a963ffb2712f07e0
Ale Soto <asoto@ltvworkingteam.com>
2025-03-27 21:48:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import federation from '@originjs/vite-plugin-federation'
 
 export default defineConfig({
+  base: 'https://alsoto25.github.io/react-components-test/',
   plugins: [
     federation({
       name: 'sharedComponents', // Unique name for this remote
